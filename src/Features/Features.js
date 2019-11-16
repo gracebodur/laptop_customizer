27569cef80c15a579b2d86af92189a8b1e96e6c9
Grace <mgracebodur@gmail.com>
2019-11-15 23:02:03 -0500

    Changed Header  into a functional based component

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -6,7 +6,7 @@ class Features extends React.Component {
         return (
             <form className="main__form">
                 <h2>Customize your laptop</h2>
-                 {/* features build in the FeatureListComponent */} */}
+                 {/* features build in the FeatureListComponent */}
                 <FeaturesList 
                 USCurrencyFormat={this.props.USCurrencyFormat} 
                 selected={this.props.selected} 
